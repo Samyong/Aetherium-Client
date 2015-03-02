1c797c01e9834b20f0f9a60a656d82a6fa1d579b
Samyong <samo.stardust@gmail.com>
2015-03-02 10:37:01 +1030

    Added Project

---
+++ Aetherium_Launcher/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aetherium_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ellipses.Stroke = new SolidColorBrush(Color.FromRgb(90, 90, 90));
+            ellipses.Fill = new SolidColorBrush(Color.FromRgb(90, 90, 90));
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ellipses.Stroke = new SolidColorBrush(Color.FromRgb(110, 110, 110));
+            ellipses.Fill = new SolidColorBrush(Color.FromRgb(110, 110, 110));
+        }
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
