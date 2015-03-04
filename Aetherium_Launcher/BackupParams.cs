f61ffe48ec4e53194f1309756f204b6a93282874
Samyong <samo.stardust@gmail.com>
2015-03-04 20:49:12 +1030

    Progress Bars altered, Downloads and Unzips
    Success!

---
+++ Aetherium_Launcher/BackupParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aetherium_Launcher
+{
+    class BackupParams
+    {
+        public string Source { get; set; }
+        public string Output { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Aetherium_Launcher/MainWindow.xaml.cs
+++ Aetherium_Launcher/MainWindow.xaml.cs
@@ -14,9 +14,14 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Web;
 using System.IO;
+//using System.IO.Compression;
 using Microsoft.TeamFoundation.WorkItemTracking.ControlsCore;
 using System.Reflection;
-
+using System.Net;
+using System.ComponentModel;
+using Ionic.Zip;
+using System.Threading;
+using System.Diagnostics;
 
 
 namespace Aetherium_Launcher
@@ -26,9 +31,18 @@ namespace Aetherium_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker worker;
+        Stopwatch sw = new Stopwatch();
         public MainWindow()
         {
             InitializeComponent();
+            checkIfExist();
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_Progress);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_Complete);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
 
         }
         public void DisableErrors(WebBrowser wb, bool Hide)
@@ -53,7 +67,7 @@ namespace Aetherium_Launcher
         {
             DisableErrors(browser, true);
         }
-
+        /*
         private void progControl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             try
@@ -76,6 +90,7 @@ namespace Aetherium_Launcher
             }
 
         }
+         * */
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -84,9 +99,82 @@ namespace Aetherium_Launcher
                 DragMove();
             }
         }
+        public bool checkIfExist()
+        {
+            if(File.Exists("Aetherium.exe") == true)
+            {
+                return true;
+            }
+            else
+            {
+                downloadFile();
+                return false;
+            }
+ 
+        }
+
+        public void downloadFile()
+        {
+            WebClient client = new WebClient();
 
 
-        //settings rotate animation
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+            //starts the download
+            sw.Start();
+            try
+            {
+                client.DownloadFileAsync(new Uri("http://aetherium.us/files/Aetherium_Launcher.zip"), "Aetherium.zip");    
+            }
+            catch
+            {
+
+            }
+     
+        }
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            progress.Text = "Downloading " + Math.Floor(percentage) + "% " + string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+            dlProgressBar.Width = Math.Floor(percentage * 666 / 100);
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                worker.ReportProgress(i);
+            }
+                using (ZipFile zip = ZipFile.Read("Aetherium.zip"))
+                {
+                    foreach (ZipEntry file in zip)
+                    {
+                        file.Extract(@"\Aetherium", ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+                worker.ReportProgress(100);
+        }
+        void worker_Progress(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Text = "Extracting " + e.ProgressPercentage.ToString() + "%";
+            unzipProgressBar.Width = e.ProgressPercentage * 341 / 100;
+        }
+        void worker_Complete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            sw.Reset();
+            progress.Text = "Extracting 100% - Completed";
+        }
+
+       
+
+
 
     }
 }
